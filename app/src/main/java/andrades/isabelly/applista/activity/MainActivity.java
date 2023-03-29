0492445654465c0edea4a31ebea180d29559aa89
Isabelly <isabellyandrades.ifes@gmail.com>
2023-03-29 08:22:41 -0300

    feito até passo 08

--- app/src/main/java/andrades/isabelly/applista/activity/MainActivity.java
+++ app/src/main/java/andrades/isabelly/applista/activity/MainActivity.java
@@ -3,6 +3,7 @@ package andrades.isabelly.applista.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
--- app/src/main/java/andrades/isabelly/applista/activity/NewItemActivity.java
+++ app/src/main/java/andrades/isabelly/applista/activity/NewItemActivity.java
@@ -1,16 +1,51 @@
 package andrades.isabelly.applista.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import andrades.isabelly.applista.R;
 
 public class NewItemActivity extends AppCompatActivity {
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelect = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+        ImageButton imgCI = findViewById(R.id.imbCl);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+
+            }
+
+        });
+    }
+    //PAREI PASSO 8
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PHOTO_PICKER_REQUEST) {
+            if(resultCode == Activity.RESULT_OK) {
+                photoSelected = data.getData();
+                ImageView imvfotoPreview = findViewById(R.id.imvPhotoPreview);
+                imvfotoPreview.setImageURI(selectPhotoLocation);
+
+            }
+        }
     }
 }
